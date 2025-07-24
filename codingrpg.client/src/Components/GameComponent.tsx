3589ff16868696ee92fbc825027dde3936cf7e4d
Matt Hunt <wmatthunt@gmail.com>
2025-07-23 20:47:38 -0400

    basic combat, hp doesn't update yet on screen

--- codingrpg.client/src/Components/GameComponent.tsx
+++ codingrpg.client/src/Components/GameComponent.tsx
@@ -1,6 +1,6 @@
 //component imports
 //model imports
-import { Hero } from "../Models/CharacterModel";
+import { Hero, Rat } from "../Models/CharacterModel";
 //react imports
 import { useState } from "react";
 //stylesheet imports
@@ -9,7 +9,9 @@ import '../StyleSheets/GameStyle.css';
 function GameComponent() {
     const [active, setActive] = useState("MainMenu");
     const [hero, setHero] = useState(new Hero("Hero"));
-    const [text,setText] = useState("Welcome to Coding RPG") 
+    const [text, setText] = useState("Welcome to Coding RPG")
+    const [enemy, setEnemy] = useState(new Rat("Rat"))
+    const [combat, setCombat] = useState(false)
 
     const handleCreateCharacter = () => {
         const inputElement = document.getElementById('name-input') as HTMLInputElement;
@@ -23,10 +25,53 @@ function GameComponent() {
             setText("Please name your character")
         }
     }
+
+    const handleCombat = () => {
+        setCombat(true)
+        setEnemy(new Rat("Rat"))
+        setActive("Combat")
+        setText(`${hero.name} VS ${enemy.name}`)
+    }
+    const handleCombatRound = () => {
+        if (hero.currentHP > 0 && enemy.currentHP > 0) {
+            console.log("combat round");
+            hero.currentHP -= enemy.weapon.power;
+            console.log(hero.currentHP)
+            enemy.currentHP -= hero.weapon.power;
+            console.log(enemy.currentHP)
+            setHero(hero)
+            setEnemy(enemy)
+        }
+        if (hero.currentHP <= 0 || enemy.currentHP <= 0)
+        {
+            setCombat(false)
+            if (hero.currentHP > 0) {
+                setText(`${hero.name} is victorious`)
+            }
+            else { setText(`${hero.name} is defeated`) }
+        }
+    }
     return (
         <div id="game">
+            {active === "Combat" ? <div>
+                <h2>Combat</h2>
+                <div>
+                    <h3>{hero.name}</h3>
+                    <p><span>HP: {hero.currentHP}/{hero.maxHP}</span></p>
+                </div>
+                <div>
+                    <h3>{enemy.name}</h3>
+                    <p><span>HP: {enemy.currentHP}/{enemy.maxHP}</span></p>
+                </div>
+                {combat === true ?
+                    <div>
+                        <button className='menu-button' onClick={() => handleCombatRound()}>Attack</button>
+                        <button className='menu-button' onClick={() => setActive("Game")}>Run</button>
+                    </div> : <div>
+                        <button className='menu-button' onClick={() => setActive("Game")}>Exit</button></div>}
+            </div> : <div></div>}
             {active === "Game" ? <div>
-              {/*  <h2>In Game - {hero.name}</h2>*/}
+                <button className='menu-button' onClick={() => handleCombat()}>Combat Test</button>
                 <button className='menu-button' onClick={() => setActive("MainMenu")}>Main Menu</button>
             </div> : <div></div>}
             {active === "LoadGame" ? <div>
@@ -35,7 +80,7 @@ function GameComponent() {
             </div> : <div></div>}
             {active === "MainMenu" ? <div id="main-menu">
                 <h2>Main Menu</h2>
-     {/*           <button className='menu-button' onClick={() => setActive("Game")}>Continue</button>*/}
+                <button className='menu-button' onClick={() => setActive("Game")}>Continue</button>
                 <button className='menu-button' onClick={() => setActive("NewGame")}>New Game</button>
                 <button className='menu-button' onClick={() => setActive("LoadGame")}>Load Game</button>
                 <button className='menu-button' onClick={() => setActive("Settings")}>Settings</button>
@@ -44,7 +89,7 @@ function GameComponent() {
             {active === "NewGame" ? <div>
                 <h2>Character Creation</h2>
                 <div>
-                    <label htmlFor="name-input">Select Character Name: <input id="name-input"></input></label>
+                    <label htmlFor="name-input">Select Character Name: <input id="name-input" ></input></label>
                     <button onClick={() => handleCreateCharacter()}>Create Character</button>
                 </div>
                 <div>
--- codingrpg.client/src/Models/CharacterModel.ts
+++ codingrpg.client/src/Models/CharacterModel.ts
@@ -11,18 +11,35 @@ export class Character {
     inventory: item[] = []
     weapon: weapon = new weapon("Bare Hand", 1);
 }
-export class Hero extends Character {
+
+export class Beast extends Character {
     constructor(name: string) {
         super()
+        this.name = name;
+    }
+}
+export class Humanoid extends Character {
+    constructor(name: string) {
+        super()
+        this.name = name;
+    }
+}
+export class Hero extends Humanoid {
+    constructor(name: string) {
+        super(name)
         this.name = name;
         this.gold = 50
     }
-}
-export class Monster extends Character {
-    constructor(name: string) {
-        super()
+} export class Rat extends Beast {
+    constructor(name: string = "Rat") {
+        super(name)
         this.name = name;
+        this.currentHP = 5;
+        this.maxHP = 5;
+        this.weapon = new weapon("Bite", 1);
+
     }
 }
+
 
 
